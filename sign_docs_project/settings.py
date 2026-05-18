7b895274700321461a5814d01e13dc60c3916637
0715173877 <cmihanjo@gmail.com>
2026-05-18 15:17:00 +0300

    new database postgres

--- sign_docs_project/settings.py
+++ sign_docs_project/settings.py
@@ -62,8 +62,12 @@ WSGI_APPLICATION = 'sign_docs_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', default='sign_docs'),
+        'USER': config('DB_USER', default='sign_docs'),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
